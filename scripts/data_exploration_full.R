14b1b09022567d329b347eceb4c7614e9e359990
DaniGargya <daniela@gargya.de>
2020-03-06 11:48:04 +0000

    Dani added vis

--- scripts/data_exploration_full.R
+++ scripts/data_exploration_full.R
@@ -22,6 +22,26 @@ library(treemapify) # for area graph
 library(ggplot2)
 library(ggpubr)
 library(mapdata)
+library(gridExtra)
+
+# setting a clean theme ----
+theme_clean <- function(){
+  theme_bw() +
+    theme(axis.text.x = element_text(size = 14),
+          axis.text.y = element_text(size = 14),
+          axis.title.x = element_text(size = 14, face = "plain"),             
+          axis.title.y = element_text(size = 14, face = "plain"),             
+          panel.grid.major.x = element_blank(),                                          
+          panel.grid.minor.x = element_blank(),
+          panel.grid.minor.y = element_blank(),
+          panel.grid.major.y = element_blank(),  
+          plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), units = , "cm"),
+          plot.title = element_text(size = 15, vjust = 1, hjust = 0.5),
+          legend.text = element_text(size = 12, face = "italic"),          
+          legend.title = element_blank(),                              
+          legend.position = c(0.5, 0.8))
+}
+
 
 # data inclusion criteria and merge datasets ----
 
@@ -105,5 +125,102 @@ write.table(samples_taxa, "outputs/samples_taxa.txt")
 
 
 # visualisation ----
+# spatial distribution of biodiversity time-series ----
+bio_short <- bio %>% 
+  distinct(STUDY_ID_PLOT, CENT_LAT, CENT_LONG, TAXA, TOTAL, START_YEAR, END_YEAR, NUMBER_OF_SAMPLES)
 
 
+(map_studies2 <- ggplot(bio_short,
+                       aes(x = CENT_LONG, y = CENT_LAT, colour = TAXA, size = NUMBER_OF_SAMPLES), alpha = I(0.7)) +
+   borders("world", colour = "gray88", fill = "gray88", size = 0.3) +
+   coord_cartesian(xlim = NULL, ylim = NULL, expand = TRUE) +
+   theme_map() +
+   geom_point(range = c(7,15)) +
+   scale_size_continuous(range = c(3,10)) +
+   guides(size = FALSE) +
+   scale_colour_brewer(palette = "Dark2") +
+   scale_fill_manual(labels = c("Terrestrial plants",
+                                "Birds",
+                                "Mammals",
+                                "Terrestrial invertebrates")) +
+   labs(title = ("\n\n a) Spatial distribution of time-series\n")) +
+   theme(legend.position= "bottom", 
+         legend.title = element_blank(),
+         legend.text = element_text(size = 14),
+         legend.justification = "top",
+         plot.title = element_text(size = 14, hjust = 0.5, face = "bold")))
+
+ggsave(map_studies2, filename = "outputs/map_studies2.png",
+       height = 5, width = 8)
+
+# temporal distribution of biodiversity time-series ----
+# making id variable as factor
+bio_short$STUDY_ID_PLOT <- as.factor(as.character(bio_short$STUDY_ID_PLOT))
+
+# create a sorting variable
+bio_short$sort <- bio_short$TAXA
+bio_short$sort <- factor(bio_short$sort, levels = c("Terrestrial plants",
+                                                "Birds",
+                                                "Mammals",
+                                                "Terrestrial invertebrates"),
+                       labels = c(1,2,3,4))
+
+
+bio_short$sort <- paste0(bio_short$sort, bio_short$START_YEAR)
+bio_short$sort <- as.numeric(as.character(bio_short$sort))
+
+(timeline_studies2 <- ggplot() +
+    geom_linerange(data = bio_short, aes(ymin = START_YEAR, ymax = END_YEAR, 
+                                       colour = TAXA,
+                                       x = fct_reorder(STUDY_ID_PLOT, desc(sort))),
+                   size = 1) +
+    scale_colour_brewer(palette = "Dark2") +
+    labs(x = NULL, y = NULL,
+         title = ("\n\n b) Temporal distribution of time-series\n")) +
+    theme_clean() +
+    coord_flip() +
+    guides(colour = F) +
+    theme_clean() +
+    theme(panel.grid.minor = element_blank(),
+          panel.grid.major.y = element_blank(),
+          panel.grid.major.x = element_line(),
+          axis.ticks = element_blank(),
+          legend.position = "bottom", 
+          panel.border = element_blank(),
+          legend.title = element_blank(),
+          axis.title.y = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks.y = element_blank(),
+          plot.title = element_text(size = 14, hjust = 0.5, face = "bold"),
+          axis.text = element_text(size = 16), 
+          axis.title = element_text(size = 20)))
+
+ggsave(timeline_studies2, filename = "outputs/timeline_studies2.png",
+       height = 5, width = 8)
+
+# taxonomic distribution of biodiversity time-series ----
+# calculating sample size for each taxa
+taxa_sum <- bio_short %>%  group_by(TAXA) %>% tally
+
+(taxa_studies2 <- ggplot(taxa_sum, aes(area = n, fill = TAXA, label = n,
+                                      subgroup = TAXA)) +
+    geom_treemap() +
+    geom_treemap_subgroup_border(colour = "white", size = 1) +
+    geom_treemap_text(colour = "white", place = "center", reflow = T) +
+    scale_colour_brewer(palette = "Dark2") +
+    scale_fill_brewer(palette = "Dark2") +
+    labs(title = ("\n\n c) Taxonomic distribution of time-series\n")) +
+    theme(plot.title = element_text(size = 14, hjust = 0.5, face = "bold")) +
+    guides(fill= FALSE))
+
+ggsave(taxa_studies2, filename = "outputs/taxa_studies2.png",
+       height = 5, width = 8)
+
+# panel ----
+panel_b2 <- ggarrange(timeline_studies2, taxa_studies2, ncol = 2, align = c("h"))
+
+
+panel_full2 <- grid.arrange(map_studies2, panel_b2, nrow = 2)
+
+ggsave(panel_full2, filename ="outputs/panel_studies2.png",
+       height = 10, width = 8)
