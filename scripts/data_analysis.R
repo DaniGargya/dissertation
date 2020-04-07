7c6c6f898bcb37a209953ff1ebd4370c562c38f0
DaniGargya <daniela@gargya.de>
2020-04-07 17:49:58 +0100

    Dani added other potential models

--- scripts/data_analysis.R
+++ scripts/data_analysis.R
@@ -31,7 +31,7 @@ sumamry(data1)
 # visualising distributions of data----
 # -> zero one inflated beta ditribution
 
-# Model with brms ----   
+# Model with all ----   
 # default priors
 # zero one inflated beta distribution
 # zoi refers to the probability of being a zero or a one
@@ -58,45 +58,59 @@ save(mo_tu, file = "outputs/mo_tu.RData")
 # predicting ----
 predictions <- ggpredict(mo_tu, terms = c("scaleacc", "scalehpd", "duration_plot_center"))
 
-#predictions$Monitoring <- factor(predictions$group, levels = c("11.44", "20.79", "30.13"),
-                                 #labels = c("Short-term", "Moderate", "Long-term"))
+predictions$hpd <- factor(predictions$group, levels = c("-0.08", "0.02", "0.13"),
+                                 labels = c("Low", "Moderate", "High"))
 
 plot(predictions)
 
-# saving model output as tabe
+# saving model output as table
 table_m <- tidy_stan(mo_tu, effects = "fixed", digits = 2) %>% print()
 
 # Create table for report
 #stargazer(table_m, type = "html", out = "outputs/table_m.html", title = "results")
 
-# visualising ----
-theme_clean <- function(){
-  theme_bw() +
-    theme(axis.text.x = element_text(size = 14),
-          axis.text.y = element_text(size = 14),
-          axis.title.x = element_text(size = 14, face = "plain"),             
-          axis.title.y = element_text(size = 14, face = "plain"),             
-          panel.grid.major.x = element_blank(),                                          
-          panel.grid.minor.x = element_blank(),
-          panel.grid.minor.y = element_blank(),
-          panel.grid.major.y = element_blank(),  
-          plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), units = , "cm"),
-          plot.title = element_text(size = 15, vjust = 1, hjust = 0.5),
-          legend.text = element_text(size = 12, face = "italic"),          
-          legend.title = element_text(size = 12, face = "bold"),                              
-          legend.position = c(0.2, 0.8))
-}
+# calculating quartiles for levels hpd ----
+dat <- data1 %>% 
+  drop_na(scalehpd)
 
-ggplot() +
-  geom_line(data = predictions, aes(x = x, y = predicted),
-            size = 2) +
-  geom_ribbon(data = predictions, aes(ymin = conf.low, ymax = conf.high, 
-                                      x = x), alpha = 0.1) +
-  geom_point(data = data1, aes(x = scaleacc, y = Jtu),
-             alpha = 0.1, size = 2) +
-  #annotate("text", x = -0.65, y = 5, label = "Slope = -0.06, Std. error = 0.01") +  
-  #scale_x_continuous(limits = c (0.8, 1)) +
-  theme_clean() +
-  #scale_fill_manual(values = c("darksalmon", "firebrick3", "firebrick4")) +
-  #scale_colour_manual(values = c("darksalmon", "firebrick3", "firebrick4")) +
-  labs(x = "\nAccessibility", y = "Jaccard turnover\n")
+quantile(dat$scalehpd)
+#           0%          25%          50%          75%         100% 
+#0.000000e+00 0.000000e+00 8.891233e-05 1.794126e-03 1.000000e+00 
+
+# "0", "8.891233e-05", "1.794126e-03"
+
+quantile(predictions$group)
+str(predictions)
+
+# model all with area ----
+mo_tu_a <- brm(bf(Jtu ~ scaleacc*scalehpd + duration_plot_center + area_center +
+                  (scaleacc|TAXA) + (1|cell) + (1|STUDY_ID), coi ~ 1, zoi ~ 1),
+             family = zero_one_inflated_beta(), 
+             data = data1,
+             iter = 2000,
+             warmup = 1000,
+             inits = '0',
+             control = list(adapt_delta = 0.85),
+             cores = 2, chains = 2)
+
+# model accessibility ----
+mo_tu_acc <- brm(bf(Jtu ~ scaleacc + duration_plot_center + area_center +
+                    (scaleacc|TAXA) + (1|cell) + (1|STUDY_ID), coi ~ 1, zoi ~ 1),
+               family = zero_one_inflated_beta(), 
+               data = data1,
+               iter = 2000,
+               warmup = 1000,
+               inits = '0',
+               control = list(adapt_delta = 0.85),
+               cores = 2, chains = 2)
+
+# model hpd ----
+mo_tu_hpd <- brm(bf(Jtu ~ scaleacc + duration_plot_center + area_center +
+                      (scaleacc|TAXA) + (1|cell) + (1|STUDY_ID), coi ~ 1, zoi ~ 1),
+                 family = zero_one_inflated_beta(), 
+                 data = data1,
+                 iter = 2000,
+                 warmup = 1000,
+                 inits = '0',
+                 control = list(adapt_delta = 0.85),
+                 cores = 2, chains = 2)
