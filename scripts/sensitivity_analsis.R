2b41b78b357a169145a4df341b229b6e22d93b9e
DaniGargya <daniela@gargya.de>
2020-04-10 12:45:55 +0100

    Dani added sensitivity analysis script

---
+++ scripts/sensitivity_analsis.R
@@ -0,0 +1,20 @@
+# Sensitivity analysis dissertation
+# Dani Gargya
+# April 2020
+
+# sites coincidence with protected areas ----
+protected_area <- bio %>% 
+  group_by(PROTECTED_AREA) %>% 
+  summarise(plots =length(unique(STUDY_ID_PLOT)))
+
+# smaller timeframe ----
+short_timeframe <- bio %>% 
+  filter(START_YEAR > 1980,
+         END_YEAR < 2015)
+
+# scale accessibility ----
+
+# differences in latitude? (more promminent in tropics?)----
+
+# number of data points?
+
