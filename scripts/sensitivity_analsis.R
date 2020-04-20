803eca0b9bcdcc7456abb6d40ce04dc0e85c0b0f
DaniGargya <daniela@gargya.de>
2020-04-20 22:11:41 +0100

    Dani added model for sensitivity analysis

--- scripts/sensitivity_analsis.R
+++ scripts/sensitivity_analsis.R
@@ -16,6 +16,28 @@ short_timeframe <- bio %>%
          END_YEAR < 2015)
 
 # scale accessibility ----
+data1 <- data1 %>% 
+  mutate(scaleacc_100= 1 - ((e_100-min(e_100))/(max(e_100)-min(e_100)))) %>% 
+  mutate(scalehpd_100= (e_hpd100-min(e_hpd100))/(max(e_hpd100)-min(e_hpd100)))
+
+check <- data1 %>% 
+  drop_na(e_hpd)
+min(check$e_hpd)
+max(check$e_hpd)
+
+hist(data1$scalehpd_100)
+hist(data1$scaleacc_25)
+
+
+mo_tu_scale100 <- brm(bf(Jtu ~ scaleacc_100 + scalehpd_100 + duration_plot + TAXA +
+                        (1|STUDY_ID)),
+                   family = zero_one_inflated_beta(), 
+                   data = data1,
+                   iter = 4000,
+                   warmup = 1000,
+                   inits = '0',
+                   control = list(adapt_delta = 0.85),
+                   cores = 2, chains = 4)
 
 # differences in latitude? (more promminent in tropics?)----
 
